26e4c22b330909631fc18cbcc66c217f406cfe89
Saksham Singh Rajput <rajputsaksham74@gmail.com>
2024-04-27 22:53:44 +0530

    name change

--- js/shop.js
+++ js/shop.js
@@ -11,7 +11,7 @@ let Productlist = document.querySelector('.productlist');
 function addDataToHTML(){
   products.forEach(product => {
     let newProduct = document.createElement('a');
-    newProduct.href = '/shop.html?id=' + product.id;
+    newProduct.href = '/Shop.html?id=' + product.id;
     newProduct.classList.add('item');
     newProduct.innerHTML = `
       <img src="${product.image}">
